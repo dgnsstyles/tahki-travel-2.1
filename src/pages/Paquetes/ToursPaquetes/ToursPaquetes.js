81d23aedba12e7762b7edd6a93e7bc3b264206f9
Web Master <webmaster@Webs-MacBook-Pro.local>
2018-05-28 09:36:11 -0400

    cambios en los panes de paquetes.js

--- src/pages/Paquetes/ToursPaquetes/ToursPaquetes.js
+++ src/pages/Paquetes/ToursPaquetes/ToursPaquetes.js
@@ -1,20 +1,67 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Container, Tab} from 'semantic-ui-react'
+import PropTypes from 'prop-types';
 import DiaUno from '../DiasComponents/DiaUno'
-import DiaDos from '../DiasComponents/DiaDos'
-import DiaTres from '../DiasComponents/DiaTres'
 
-const panes = [
-    { menuItem: 'Dia 1', render: () => <Tab.Pane attached={false}><DiaUno/></Tab.Pane>},
-    { menuItem: 'Dia 2', render: () => <Tab.Pane attached={false}><DiaDos/></Tab.Pane>},
-    { menuItem: 'Dia 3', render: () => <Tab.Pane attached={false}><DiaTres/></Tab.Pane>}
-]
+class ToursPaquetes extends Component {
 
-const TourPaquetes = (props) =>(
+    constructor(props){
+        super(props);
+        this.state = {
+            tabs: [],
+            data: null
+        }
+    }
 
-    <Container>
-        <Tab menu={{ secondary: true, pointing: true}} panes={panes}/>
+    componentDidMount(){
 
-    </Container>
-)
-export default TourPaquetes
+        var t = []
+        var self = this;
+
+        var inter = setInterval( function () {
+            if(self.props.data != undefined && self.props.data !=null && self.props.data.length !=0){
+                clearInterval(inter);
+                var obj = self.filtrar();
+                var inter2 =  setInterval(function(){
+                    if(obj !=null && obj.length !=0){
+                        clearInterval(inter2);
+                        obj[0].days.map((element, index) => {
+                            t.push({ menuItem: 'Dia ' + (index + 1), render: () => <Tab.Pane attached={false}><DiaUno data={element}/></Tab.Pane>});
+                        })
+
+                        self.setState({
+                            data: obj
+                        })
+                        self.setState({
+                            tabs: t
+                        })
+                    }
+                }, 100)
+            }
+
+        }, 1000)
+    }
+
+    filtrar(){
+        return this.props.data.filter(e =>{
+            if(Number(e.commercial_id) === Number(this.props.param.id)){
+                return e;
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Container>
+                <Tab menu={{ secondary: true, pointing: true}} panes={this.state.tabs}/>
+            </Container>
+        );
+    }
+}
+
+ToursPaquetes.propTypes = {};
+
+export default ToursPaquetes;
+
+
+
